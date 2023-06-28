f929a0f8814a4981be95ec822c84800be47db198
Thanasis-pap <thanasis.papias@gmail.com>
2023-06-28 09:28:41 +0300

    Added animations with js

--- script.js
+++ script.js
@@ -34,3 +34,18 @@ function validateForm() {
   }
 }
 
+function fadeInUp() {
+  var reveals = document.querySelectorAll(".fadeInUp");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", fadeInUp);
