b1a1137ef730f3c91fad56f973b4fba7a98f6bb9
Thanasis-pap <thanasis.papias@gmail.com>
2023-06-28 10:49:01 +0300

    Added fade in

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// This script validates the contact form fields.
+// This function validates the contact form fields.
 // In order  to send the form to an e-mail, we use the https://formspree.io forms webpage.
 // That's because GitHub Pages does not support server-side code, nor can we store the 
 // reciever's email data privately.
@@ -34,12 +34,14 @@ function validateForm() {
   }
 }
 
-function fadeInUp() {
-  var reveals = document.querySelectorAll(".fadeInUp");
+/* Animations */
+
+function fadeIn() {
+  var reveals = document.querySelectorAll(".fadeIn");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 100;
+    var elementVisible = 50;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
@@ -48,4 +50,19 @@ function fadeInUp() {
   }
 }
 
+function fadeInUp() {
+  var reveals = document.querySelectorAll(".fadeInUp");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("load", fadeIn);
 window.addEventListener("scroll", fadeInUp);
