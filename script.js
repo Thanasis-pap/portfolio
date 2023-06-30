aaf32fee5f0c9f20fa678c61e5ead44d4d2fd12f
Thanasis-pap <thanasis.papias@gmail.com>
2023-06-30 20:06:12 +0300

    Added license

--- script.js
+++ script.js
@@ -1,65 +1,45 @@
-// This function validates the contact form fields.
-// In order  to send the form to an e-mail, we use the https://formspree.io forms webpage.
-// That's because GitHub Pages does not support server-side code, nor can we store the 
-// reciever's email data privately.
-
-function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
-
-function validateForm() {
-  var senderName = document.forms["fs-frm"]["full-name"].value;
-  var email = document.forms["fs-frm"]["email-address"].value;
-  var message = document.forms["fs-frm"]["message"].value;
-  var error = "";
-
-  if (senderName == "") {
-    error += "Please provide your name.\n";
-  }
-
-  if (email == "") {
-    error += "Please provide your email address.\n";
-  } else if (!validateEmail(email)) {
-    error += "Please enter a valid email address.\n";
-  }
-
-  if (message == "") {
-    error += "Please enter a message.\n";
-  }
-
-  if (error != "") {
-    alert(error);
-    return false;
-  }
-}
+/**
+ * Copyright 2023 Athanasios Papias
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /* Animations */
 
 function fadeIn() {
-  var reveals = document.querySelectorAll(".fadeIn");
-  for (var i = 0; i < reveals.length; i++) {
+  var fades = document.querySelectorAll(".fadeIn");
+  for (var i = 0; i < fades.length; i++) {
     var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementTop = fades[i].getBoundingClientRect().top;
     var elementVisible = 50;
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      fades[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active");
+      fades[i].classList.remove("active");
     }
   }
 }
 
 function fadeInUp() {
-  var reveals = document.querySelectorAll(".fadeInUp");
-  for (var i = 0; i < reveals.length; i++) {
+  var fades = document.querySelectorAll(".fadeInUp");
+  for (var i = 0; i < fades.length; i++) {
     var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementTop = fades[i].getBoundingClientRect().top;
     var elementVisible = 150;
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      fades[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active");
+      fades[i].classList.remove("active");
     }
   }
 }
